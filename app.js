cee8c61d21a1dd23b4debd950cfc058a28bf4f79
MiraVanderjeugt <mira.vanderjeugt@gmail.com>
2019-10-16 11:23:00 +0200

    Database antwerpen in console gebracht

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require ('express');
 const path = require('path');
+const request = require('request');
 const app = express();
 const port = 3000;
+
+var data;
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
@@ -9,8 +12,24 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', {
+    scholen: data
+  });
 });
 
-
 app.listen(port);
+
+
+request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek1/MapServer/8/query?where=1%3D1&outFields=OBJECTID,OBDD,CATEGORIE,PUBLICEREN,OMSCHRIJVING,EXTRA_INFO_PUBLIEK,VRIJSTAAND,TYPE,STADSEIGENDOM,BETALEND,STRAAT,HUISNUMMER,POSTCODE,DISTRICT,BEHEERDER,CONTACTPERSOON,CONTACTGEGEVENS,INTEGRAAL_TOEGANKELIJK,LUIERTAFEL,Y_COORD,ID,X_COORD,OPENINGSUREN_OPM&outSR=4326&f=json',
+  function(error, response, body){
+    data = JSON.parse(body);
+
+    for(var i=0; i < data.features.length; i++) {
+        /*console.log("naam: " + data.features[i].attributes.naam);
+        console.log("coord: " + data.features[i].geometry.x + ", " + data.features[i].geometry.y);
+        console.log("");*/
+        console.log(data.features[i].attributes);
+    }
+
+  }
+);
