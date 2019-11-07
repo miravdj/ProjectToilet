4c89f871f1617a2dd6ac45cf0d1b45a74f72b194
MiraVanderjeugt <mira.vanderjeugt@gmail.com>
2019-11-07 09:53:46 +0100

    Hamburger toegevoegd. + fix van map

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -22,3 +22,14 @@ function onLocationError(e) {
 }
 
 map.on('locationerror', onLocationError);
+
+
+// navigatie
+(function(){
+
+$(".hamburger").on("click", function() {
+	$(this).parent(".hamburger-wrapper").toggleClass("hamburger-active")
+  $('#map').toggleClass("hamburger-active-onmap");
+});
+
+}());
