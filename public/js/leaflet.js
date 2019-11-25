b4d3fc322631d6db154761e05e2e665d606196ca
MiraVanderjeugt <mira.vanderjeugt@gmail.com>
2019-11-25 10:35:37 +0100

    map werkt weer. Layers niet

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -35,23 +35,3 @@ const toiletplaats = L.icon({
 });
 
 // layergroups
-
-/*$.getJSON("/ap.json", function(json) {
-  const toiletten = json.toiletten;
-  for (var i=0; i<toiletten.length; i++) {
-    new L.marker(toiletten[i].X_COORD, toiletten[i].Y_COORD, {
-        icon: toiletimg
-      }).bindPopup(`
-        <img src="${toiletten[i].marker}" alt="Toilet marker" class="popup-marker">
-        <h1>Campus ${toiletten[i].OMSCHRIJVING}</h1>
-        <h2>${toiletten[i].BETALEND}</h2>
-        <p>
-        ${toiletten[i].STRAAT} ${toiletten[i].number} <br>
-        ${toiletten[i].POSTCODE} ${toiletten[i].city} <br>
-        ${toiletten[i].DISTRICT} <br>
-        </p>
-        `)
-      .addTo(campusMap);
-   }
-}
-})*/
