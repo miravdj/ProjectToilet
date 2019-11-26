0e0836d767d4b7df6fd2d27cade393d5f09924c2
Lynn <lynn.theuns@student.ap.be>
2019-11-26 15:21:49 +0100

    marker van de locatie aangepast, naam: locatieicoon

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -3,13 +3,36 @@ var map = L.map('map').fitWorld();
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+/*
+const toiletimg = L.icon({
+  iconUrl: '/img/markertoilet.png',
+  iconSize: [35, 50],
+  iconAnchor: [0, 0],
+  popupAnchor: [0, 0]
+});
+
+const toiletplaats = L.icon({
+  iconUrl: '/img/locatieicoon.png',
+  iconSize: [50, 50],
+  iconAnchor: [0, 0],
+  popupAnchor: [,0]
+});
+*/
+
+//icon locatieicoon
+var locatieicoon = L.icon({
+    iconUrl: 'img/locatieicoon.png',
+    iconSize: [38, 50],
+    iconAnchor: [22, 34],
+    popupAnchor: [-3, -76],
+});
 
 map.locate({setView: true, maxZoom: 16});
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
 
-    L.marker(e.latlng).addTo(map)
+    L.marker(e.latlng,{icon:locatieicoon}).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
     L.circle(e.latlng, radius).addTo(map);
 }
@@ -20,16 +43,3 @@ function onLocationError(e) {
     alert(e.message);
 }
 map.on('locationerror', onLocationError);
-
-const toiletimg = L.icon({
-  iconUrl: '/img/markertoilet.png',
-  iconSize: [35, 50],
-  iconAnchor: [0, 0],
-  popupAnchor: [0, 0]
-});
-const toiletplaats = L.icon({
-  iconUrl: '/img/marker.png',
-  iconSize: [50, 50],
-  iconAnchor: [0, 0],
-  popupAnchor: [,0]
-});
