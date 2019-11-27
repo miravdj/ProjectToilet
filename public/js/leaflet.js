46d29c75c45b86595cd85a5aa36cae083418792e
UlkerOruc <oruc.ulker@student.ap.be>
2019-11-27 18:45:21 +0100

    Toevoeging van layers

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -43,3 +43,16 @@ function onLocationError(e) {
     alert(e.message);
 }
 map.on('locationerror', onLocationError);
+
+
+
+// toevoeging van layers
+var littleton = L.marker([, ]);
+
+var man = L.layerGroup([littleton]);
+
+var overlayMaps = {
+    "mannen": man
+};
+
+L.control.layers(overlayMaps).addTo(map);
