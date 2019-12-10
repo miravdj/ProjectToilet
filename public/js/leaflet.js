1c4a50ec4329bf0c4961a0854d40d968d7e99b9b
MiraVanderjeugt <mira.vanderjeugt@gmail.com>
2019-12-10 11:35:53 +0100

    greyscale

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,6 +1,6 @@
 var map = L.map('map').fitWorld();
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
