da8a80224433fd0993d925a66d456c631c3699f7
MiraVanderjeugt <mira.vanderjeugt@gmail.com>
2019-12-10 11:29:40 +0100

    greyscale

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,8 +1,9 @@
 var map = L.map('map').fitWorld();
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
 
 const toiletimg = L.icon({
   iconUrl: '/img/markertoilet.png',
